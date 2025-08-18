413b13886ce498be34cd99d4f2a19331087c9c70
jiaying.yang <jiaying.yang@qq.com>
2025-08-18 18:30:17 +0800

    add bili

--- config/platforms.js
+++ config/platforms.js
@@ -3,7 +3,8 @@ export const platforms = [
   { value: 'xiaohongshu', label: '小红书', tutorialLink: 'https://blog.notionedu.com/article/20e73415-a279-80d5-81ce-d87b9884cc14' },
   { value: 'weread', label: '微信读书', tutorialLink: 'https://blog.notionedu.com/article/22d73415-a279-801c-8b4e-f2503e0ef65b' },
   { value: 'flomo', label: 'flomo', tutorialLink: 'https://blog.notionedu.com/article/23a73415-a279-802f-a5bd-f15b1b5a8622' },
-  { value: 'jike', label: '即刻', tutorialLink: 'https://blog.notionedu.com/article/24673415-a279-80bf-b146-f7fd2cfb368e' }
+  { value: 'jike', label: '即刻', tutorialLink: 'https://blog.notionedu.com/article/24673415-a279-80bf-b146-f7fd2cfb368e' },
+  { value: 'bili', label: 'B站', tutorialLink: 'https://blog.notionedu.com/article/25273415-a279-80b0-88f1-ec0a9e1c5772' }
 ]
 
 // 获取平台显示名称
